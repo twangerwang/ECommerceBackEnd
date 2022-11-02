7efbb9a849fc246c3bee4a751d9e6a114252432c
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-11-02 02:03:11 -0500

    filled out routes and models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoInccrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {model: 'product', key: 'id'}
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {model: 'tag', key: 'id'}
+    }
+    
   },
   {
     sequelize,
